faf3580f420d5c8dd07bde4a455fadc205e0042c
Amanskywalker <ak47su30ac@gmail.com>
2016-10-16 12:26:47 +0530

    Now instructions can be detected  	modified:   shell.php

--- shell.php
+++ shell.php
@@ -1,4 +1,7 @@
 <?hh
+  // fuctions are modulated for easy structuring of the programme
+  include_once('Registers.php');
+  include_once('Functions.php');
   // these are the default variables
 
   $Start = 0000;
@@ -7,6 +10,19 @@
   $OpCode = array(
                   'LDA' => 00000000,
                 );
+  /*
+  function LDA($value)
+  {
+    $temp = explode($value,' ');
+    $Register['A']['Value'] = $temp['1'];
+    var_dump($temp);
+    var_dump($Register);
+  }
+  */
+  $Instruction = array(
+    'LDA' => array('Formate' => '3,4', 'Opcode' => 00, 'Example' => 'A <-- (m..m+2)', 'Function' => 'sddfdfs' ),
+    'LDB' => array('Formate' => '3,4', 'Opcode' => 68, 'Example' => 'B <-- (m..m+2)', 'Function' => 'sddfdfs' )
+    );
 
   while ($ContinueWhile)
   {
@@ -15,7 +31,7 @@
 
     echo "$line\n";
 
-    if ( preg_match('/[sS][tT][aA][rR][tT]/',$line))
+    if (preg_match('/[sS][tT][aA][rR][tT]/',$line))
     {
       $Chars = preg_split('/ /',$line);
       $Start = $Chars['2'];
@@ -26,6 +42,29 @@
     // to exit the intrepter
     if($line === 'exit')
       $ContinueWhile =  false;
+
+    // now searching the instructions
+
+    $temp = preg_split('/ /',$line);
+    //var_dump($temp);
+    //echo $temp['0']."\n";
+
+    // to check whether the Instruction exist or not
+    if(array_key_exists($temp['0'], $Instruction));
+    {
+      // If instruction exist then procede to execute the task specified
+      var_dump($Instruction[$temp['0']]);
+    }
+    else
+    {
+        echo "Invalid Instruction ".$temp['0'];
+    }
+
+    // set to new line for inputting the next command
     echo "\n";
   }
   print_r(readline_info());
+
+/*
+  LDA    LENGTH
+*/
