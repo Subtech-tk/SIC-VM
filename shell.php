c6c680ed72d45da55f07939743a27fe763d8c00d
Amanskywalker <ak47su30ac@gmail.com>
2016-10-22 18:42:01 +0530

    Added PC counter updation with every instruction executed   added Size of DataTypes fields   added   modified:   shell.php

--- shell.php
+++ shell.php
@@ -6,16 +6,12 @@
       Report errors on https://github.com/Subtech-tk/SIC-VM
   ";
   // fuctions are modulated for easy structuring of the programme
-  include_once('Registers.php');
+  //include_once('Registers.php');
   include_once('Functions.php');
   // these are the default variables
 
   $Start = 0000;
   $ContinueWhile = true;
-  // OPcodes stored in binary formate
-  $OpCode = array(
-                  'LDA' => 00000000,
-                );
   /*
   function LDA($value)
   {
@@ -27,13 +23,13 @@
   */
   // temprory data to be for testing purposes Actual Instructions and DataTypes are stored in Instruction_Set_Tables.php
   $Instruction = array(
-    'LDA' => array('Formate' => '3,4', 'Opcode' => 00, 'Example' => 'A <-- (m..m+2)', 'Function' => 'LDA', 'NumberOfOperands' => 1),
-    'LDB' => array('Formate' => '3,4', 'Opcode' => 68, 'Example' => 'B <-- (m..m+2)', 'Function' => 'LDB' )
+  'LDA' => array('Formate' => '3,4', 'Opcode' => 00, 'Example' => 'A <-- (m..m+2)', 'Function' => 'LDA', 'NumberOfOperands' => 1, 'InstructionSize' => 3 /*bytes*/ ),
+  'LDB' => array('Formate' => '3,4', 'Opcode' => 68, 'Example' => 'B <-- (m..m+2)', 'Function' => 'LDB', 'NumberOfOperands' => 1, 'InstructionSize' => 3 /*bytes*/ )
     );
 
   $DataTypes = array(
     'WORD' => array('MemorySize' => 16),
-    'RESW' => array('MemorySize' => 16)
+    'RESW' => array('MemorySize' => 16) /* Reserve a array*/
   );
 
   while ($ContinueWhile)
@@ -41,7 +37,7 @@
     $line = readline("$ -> ");
     var_dump($line);
 
-    echo "$line\n";
+    //echo "$line\n";
 
     if (preg_match('/[sS][tT][aA][rR][tT]/',$line))
     {
@@ -49,6 +45,8 @@
       $Start = $Chars['2'];
       //var_dump($Chars);
       echo "Start set to $Start";
+      // set PC to the start position
+      $Register['PC']['CurrentValue'] = $Start;
 
     }
     // to exit the intrepter
@@ -61,10 +59,11 @@
     if($line === 'version' || preg_match('/-[vV]/',$line))
       echo $Version_Details;
 
-
+    // Dump registers Details
     if (preg_match('/[rR][eE][gG][iI][sS][tT][eE][rR]/',$line))
     {
       print_r($Register);
+      print_r($Variables);
     }
     // now searching the instructions
 
@@ -77,12 +76,16 @@
     {
       // If instruction exist then procede to execute the task specified
       var_dump($Instruction[$temp['0']]);
-      var_dump(call_user_func($Instruction[$temp['0']]['Function'], $temp['1']));
+      // Incrementing the PC register according to the register
+      $Register['PC']['CurrentValue'] = $Register['PC']['CurrentValue'] + $instruction[$temp['0']]['InstructionSize'];
+      // calling respective function definde in Function.php
+      call_user_func($Instruction[$temp['0']]['Function'], $temp['1']);
+      var_dump($Register['A']);
     }
     else if (array_key_exists($temp['1'], $DataTypes))
     {
       var_dump($DataTypes[$temp['1']]);
-      $Variables[$temp['0']] = array('DataTypes' => $temp['1'], 'Value' => $temp['2'], 'Location' => 0);
+      $Variables[$temp['0']] = array('DataTypes' => $temp['1'], 'CurrentValue' => $temp['2'], 'Location' => 0);
       //array_push($Variables, );
       var_dump($Variables);
     }
@@ -92,7 +95,3 @@
     echo "\n";
   }
   print_r(readline_info());
-
-/*
-  LDA    LENGTH
-*/
