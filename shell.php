d7949c9a55ab703fd9636794f7f6b3ecbe74efa5
Amanskywalker <ak47su30ac@gmail.com>
2016-10-22 19:03:18 +0530

    Added else if condition to match the commands  	modified:   shell.php

--- shell.php
+++ shell.php
@@ -4,7 +4,8 @@
       Version 0.0.1 alpha
       Too buggy
       Report errors on https://github.com/Subtech-tk/SIC-VM
-  ";
+" ;
+  echo "$Version_Details";
   // fuctions are modulated for easy structuring of the programme
   //include_once('Registers.php');
   include_once('Functions.php');
@@ -12,15 +13,7 @@
 
   $Start = 0000;
   $ContinueWhile = true;
-  /*
-  function LDA($value)
-  {
-    $temp = explode($value,' ');
-    $Register['A']['Value'] = $temp['1'];
-    var_dump($temp);
-    var_dump($Register);
-  }
-  */
+
   // temprory data to be for testing purposes Actual Instructions and DataTypes are stored in Instruction_Set_Tables.php
   $Instruction = array(
   'LDA' => array('Formate' => '3,4', 'Opcode' => 00, 'Example' => 'A <-- (m..m+2)', 'Function' => 'LDA', 'NumberOfOperands' => 1, 'InstructionSize' => 3 /*bytes*/ ),
@@ -28,8 +21,8 @@
     );
 
   $DataTypes = array(
-    'WORD' => array('MemorySize' => 16),
-    'RESW' => array('MemorySize' => 16) /* Reserve a array*/
+    'WORD' => array('MemorySize' => 16,'InstructionSize' => 3 /*bytes*/),
+    'RESW' => array('MemorySize' => 16,'InstructionSize' => 3 /*bytes*/) /* Reserve a array*/
   );
 
   while ($ContinueWhile)
@@ -50,46 +43,44 @@
 
     }
     // to exit the intrepter
-    if($line === 'exit')
+    if(preg_match('/[eE][xX][iI][tT]/',$line))
       $ContinueWhile =  false;
-
-    if($line === 'help')
+    else if(preg_match('/[hH][eE][lL][pP]/',$line) || preg_match('/-[hH]/',$line))
       echo "Help";
-
-    if($line === 'version' || preg_match('/-[vV]/',$line))
+    else if(preg_match('/[vV][eE][rR][sS][iI][oO][nN]/',$line) || preg_match('/-[vV]/',$line))
       echo $Version_Details;
-
     // Dump registers Details
-    if (preg_match('/[rR][eE][gG][iI][sS][tT][eE][rR]/',$line))
+    else if (preg_match('/[rR][eE][gG][iI][sS][tT][eE][rR]/',$line))
     {
       print_r($Register);
       print_r($Variables);
     }
     // now searching the instructions
-
-    $temp = preg_split('/ /',$line);
-    //var_dump($temp);
-    //echo $temp['0']."\n";
-
-    // to check whether the Instruction exist or not
-    if(array_key_exists($temp['0'], $Instruction))
+    else
     {
-      // If instruction exist then procede to execute the task specified
-      var_dump($Instruction[$temp['0']]);
-      // Incrementing the PC register according to the register
-      $Register['PC']['CurrentValue'] = $Register['PC']['CurrentValue'] + $instruction[$temp['0']]['InstructionSize'];
-      // calling respective function definde in Function.php
-      call_user_func($Instruction[$temp['0']]['Function'], $temp['1']);
-      var_dump($Register['A']);
-    }
-    else if (array_key_exists($temp['1'], $DataTypes))
-    {
-      var_dump($DataTypes[$temp['1']]);
-      $Variables[$temp['0']] = array('DataTypes' => $temp['1'], 'CurrentValue' => $temp['2'], 'Location' => 0);
-      //array_push($Variables, );
-      var_dump($Variables);
-    }
+      $temp = preg_split('/ /',$line);
+      //var_dump($temp);
+      //echo $temp['0']."\n";
 
+      // to check whether the Instruction exist or not
+      if(array_key_exists($temp['0'], $Instruction))
+      {
+        // If instruction exist then procede to execute the task specified
+        var_dump($Instruction[$temp['0']]);
+        // Incrementing the PC register according to the register
+        $Register['PC']['CurrentValue'] = $Register['PC']['CurrentValue'] + $Instruction[$temp['0']]['InstructionSize'];
+        // calling respective function definde in Function.php
+        call_user_func($Instruction[$temp['0']]['Function'], $temp['1']);
+        var_dump($Register['A']);
+      }
+      else if (array_key_exists($temp['1'], $DataTypes))
+      {
+        var_dump($DataTypes[$temp['1']]);
+        $Variables[$temp['0']] = array('DataTypes' => $temp['1'], 'CurrentValue' => $temp['2'], 'Location' => 0);
+        //array_push($Variables, );
+        var_dump($Variables);
+      }
+    }
 
     // set to new line for inputting the next command
     echo "\n";
